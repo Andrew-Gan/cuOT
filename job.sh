424951a6ed0ed811bfe2857f254975da4f830b55
Yi En Gan <gan35@gilbreth-fe00.rcac.purdue.edu>
2023-04-24 14:45:34 -0400

    job

--- job.sh
+++ job.sh
@@ -9,12 +9,12 @@ cd $SLURM_SUBMIT_DIR
 
 make
 
-# ./pprf 11 75 >> out
-# ./pprf 14 73 >> out
-# ./pprf 17 72 >> out
-# ./pprf 20 70 >> out
-# ./pprf 23 69 >> out
-# ./pprf 26 67 >> out
+./pprf 11 75 >> out
+./pprf 14 73 >> out
+./pprf 17 72 >> out
+./pprf 20 70 >> out
+./pprf 23 69 >> out
+./pprf 26 67 >> out
 
 nsys profile --stats=true --output=nsys-stats ./pprf 14 73 > out-nsys
 
