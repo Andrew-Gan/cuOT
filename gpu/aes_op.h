f93e60903a244aa028bf9e008391b032b788d112
Zhirui Hou <hou125@purdue.edu>
2024-03-04 14:21:55 -0500

    Added optimized aesEncrypt

--- gpu/aes_op.h
+++ gpu/aes_op.h
@@ -3,8 +3,14 @@
 
 #include "gpu_define.h"
 
+#define T_TABLE_SIZE 256
+#define AES_BLOCK_SIZE 16
+#define AES_NUM_ROUNDS 10
+#define AES_RK_SIZE (AES_BLOCK_SIZE * (AES_NUM_ROUNDS+1))
+#define GPU_SHARED_MEM_BANK 32
+
 __global__
-void aesEncrypt128(uint32_t *key, uint32_t *data);
+void aesEncrypt128(uint32_t* rk, uint32_t* data);
 
 __global__
 void aesDecrypt128(uint32_t *key, uint32_t *data);
